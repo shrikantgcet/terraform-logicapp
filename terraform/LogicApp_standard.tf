9d48e23878c4e8134c737a412ca1477f2dbfb102
Shri Kant <shri.kant@tewhatuora.govt.nz>
2024-04-30 10:55:21 +1200

    folder updated

--- terraform/LogicApp_standard.tf
+++ terraform/LogicApp_standard.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "example" {
   name     = "azure-functions-test-rg"
   location = "West Europe"
 }
-
+/*
 resource "azurerm_storage_account" "example" {
   name                     = "functionsapptestsa006"
   resource_group_name      = azurerm_resource_group.example.name
@@ -15,7 +15,7 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-/*
+
 resource "azurerm_app_service_plan" "example" {
   name                = "azure-functions-test-service-plan"
   location            = azurerm_resource_group.example.location
